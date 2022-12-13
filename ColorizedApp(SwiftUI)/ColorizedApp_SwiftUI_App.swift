89484b76b7268344e60809dd5e6e79e9627b7baf
Artem LAPOV <artemlapov@me.com>
2022-12-13 11:39:32 +0100

    Initial Commit

---
+++ ColorizedApp(SwiftUI)/ColorizedApp_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUI_App.swift
+//  ColorizedApp(SwiftUI)
+//
+//  Created by Artem Lapov on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
